f34fc3524f0a24addda72b8e1cffdeedfaeb7958
Jevin Joy <jjoy19@colaistephadraig.com>
2022-12-07 03:12:07 +0000

    Audio Research qs done, Truncation complete, AI successfully duped

--- Aural/AudioGen.py
+++ Aural/AudioGen.py
@@ -1,75 +1,44 @@
 import numpy as np
-import pyaudio
 import random
-<<<<<<< HEAD
-from scipy.io.wavfile import write
 from pydub import AudioSegment
-
-p = pyaudio.PyAudio()
-
-sound1 = pydub.AudioSegment.from_file("path")
-
-volume = 0.25
-duration = len(sound1) * 1000
-
-samples = ()
-for x in range(random.randint(0, 5)):
-    fs = 44100
-    f = random.randint(440, 770)
-
-    try:
-        samples += (np.sin(random.random() * random.randint(1, 9) * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
-    except:
-        samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
-
-# write(f'/home/paradox/Documents/VSCODE Projects/BTYSTE-2023/Aural/Low.wav', fs, samples)
-output_bytes = (volume * samples).tobytes()
-sound2 = AudioSegment(data=output_bytes, sample_width=4, framerate=44100)
-=======
-from scipy.io.wavfile import write, read
-from pydub import AudioSegment
-from pydub.playback import play
 import io
 
-p = pyaudio.PyAudio()
+audioFile = "/home/paradox/Documents/VSCODE Projects/BTYSTE-2023/Aural/Datasets/Bird-Sounds/xeno-canto-ca-nv/XC7426.mp3"
+audioFile2 = "/home/paradox/Documents/VSCODE Projects/BTYSTE-2023/Aural/temp.wav"
+output = "/home/paradox/Documents/VSCODE Projects/BTYSTE-2023/Aural/Research-Tests/Stochastic/5.wav"
+sound1 = AudioSegment.from_file(audioFile)
+sound1.export(audioFile2, format="wav")
 
-stochasLevel = {"1": (20, 0), "2": (18, 0.5), "3": (5, 2), "4": (-10, 50)}
-stochasLevelID = "1"
-
-sound1 = AudioSegment.from_file("C:\\Users\\joyje\\VSCODE\\BTYSTE\\Aural\\Datasets\\notes_v2\\A\\A1.wav")
-sound1 += stochasLevel[stochasLevelID][0]
-sound1.export("Aural\\temp.wav", format="wav")
-
-sound = np.frombuffer(open("Aural\\temp.wav", "rb").read(), dtype=np.int16, count=len(open("Aural\\temp.wav", "rb").read())//2, offset=0)
-print(sound)
+sound = np.frombuffer(open(audioFile2, "rb").read(), dtype=np.float16)
 
 
 
-volume = 0.1 * stochasLevel[stochasLevelID][1]
+volume1 = 0.001
+volume2 = 1
 duration = len(sound1) / 1000
 print(duration)
 
 samples = ()
-fs = 44100
+fs = sound1.frame_rate
 for x in range(random.randint(1, 200)):
-    f = random.randint(220, 1100)
+    f = random.randint(440, 770)
 
     try:
-        samples += (np.sin(random.random() * random.randint(0, 0) * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+        samples += (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float16)
     except:
-        samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+        samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float16)
 
-print(samples)
 
 # write(f'Aural/Low.wav', fs, samples)
-sound2_bytes = (volume * samples)
-output_bytes = sound2_bytes + sound[:-(np.abs(len(sound2_bytes) - len(sound)))]
+sound2_bytes = (volume1 * samples)
+print(sound2_bytes)
+print(volume2 * sound[:-(np.abs(len(sound2_bytes) - len(sound)))])
+
+output_bytes = sound2_bytes + (volume2 * sound[:-(np.abs(len(sound2_bytes) - len(sound)))])
 output_bytes = output_bytes.tobytes()
 s = io.BytesIO(output_bytes)
-sound2 = AudioSegment.from_raw(s, sample_width=sound1.sample_width, frame_rate=44100, channels=sound1.channels)
-final_sound = sound2.overlay(sound1, position=0)
-play(sound2)
+final_sound_full = AudioSegment.from_raw(s, sample_width=sound1.sample_width, frame_rate=fs, channels=sound1.channels)
+final_sound = final_sound_full[:len(final_sound_full)]
+final_sound.export(output, format="wav")
 
-
->>>>>>> fb09d5db5 (AudioGen for research done, number seq done)
 
--- Numerical/NumGen.py
+++ Numerical/NumGen.py
@@ -3,15 +3,6 @@ import numpy
 import matplotlib.pyplot as plt
 data = []
 lx = []
-<<<<<<< HEAD
-for i in numpy.arange(0, 10, 0.065):
-
-    y = 2**i
-    x = random.randint(1,9)
-    lx.append(i)
-    z = y+x
-    data.append(z)
-=======
 
 for i in numpy.arange(0, 10, 1.5):
 
@@ -26,12 +17,7 @@ f.write(f"{str(data)[1:-1]}, Tangent\n")
 f.close()
 
 
->>>>>>> fb09d5db5 (AudioGen for research done, number seq done)
 plt.plot(lx, data, marker="o")
 plt.xlabel("input")
 plt.ylabel("f(x)")
 plt.show()
-<<<<<<< HEAD
-print(data)
-=======
->>>>>>> fb09d5db5 (AudioGen for research done, number seq done)
